66b0e5126ea546a11a6e4d57df762c722463bc8a
Alex Akinin <lksndrakinin@gmail.com>
2026-03-05 16:08:01 +0300

    Add tools to calc spatial period in image

---
+++ home_accuracy_01_take_images.py
@@ -0,0 +1,33 @@
+import time
+
+from src.GCodeSender import GCodeSenter, SomeGCodes, DEVICES
+from src.Camera import Camera
+
+
+initial_commands = [
+    SomeGCodes.SET_STEPS_PER_UNIT_GCODE,
+    SomeGCodes.SET_CURRENT_LIMIT_GCODE,
+    SomeGCodes.SET_MAX_FEEDRATE_GCODE,
+    SomeGCodes.SET_ACCELERATION_GCODE,
+    SomeGCodes.SET_MICROSTEPPING_GCODE,
+    SomeGCodes.HOME_X_GCODE,
+]
+
+sender = GCodeSenter(DEVICES[0])
+sender.connect()
+
+camera = Camera()
+
+for gcode in initial_commands:
+    response = sender.send_command(gcode)
+    print(f"Response: {response}")
+
+for i in range(100):
+    sender.send_command(SomeGCodes.HOME_X_GCODE)
+
+    time.sleep(0.3)
+
+    camera.save_image("images/home_accuracy", "image", "png")
+
+sender.close()
+camera.close()
---
+++ home_accuracy_02_analyze_images.py
@@ -0,0 +1,4 @@
+from src.FourierImageAnalyzer import main_single_run, main_multiple_run
+
+main_single_run()
+main_multiple_run()
